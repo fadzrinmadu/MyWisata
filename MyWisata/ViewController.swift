8c55040ef5954b2e85df8f04fc16a45dfe3aef67
Muhammad Fadzrin Madu <muhfadzrinmadu@gmail.com>
2022-11-02 00:51:12 +0700

    Initial Commit

---
+++ MyWisata/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWisata
+//
+//  Created by Mac Mini on 02/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
